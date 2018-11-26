187e25fae21bb3e3e600cc1ca62d57a6ff8ccd5d
aloksingh3112 <aloksingh3112@gmail.com>
2018-11-27 00:20:03 +0530

    added presentation component

---
+++ src/app/presentational/presentation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector:'app-pres',
+  templateUrl:'./presentation.component.html',
+  styleUrls:[]
+
+})
+export class PresentationComponent {
+
+}
